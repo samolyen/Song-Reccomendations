f19e9a4ec2a7ae640105bd402a3ff8e8ee695fb4
Sam! <139657947+samolyen@users.noreply.github.com>
2023-07-15 15:37:09 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+// Declare your genre arrays here
+
+// Make sure to declare your HTML elements as variables! 
+let catagorites = (".catagories");
+let reccomendations = (".reccomendations");
+let results = (".results");
+let screen2 = (".screen2");
+// Submit Button 
+let buttonstart = document.querySelector(".buttonstart");
+buttonstart.onclick = function() {
+    screen2.style.display = "none";
+
+};
+
+
+
+
+let submitButton = document.querySelector(".submitButton");
+submitButton.onclick = function() {};
